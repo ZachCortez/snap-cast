8b01b8ac54716e7e2bc450feb86253ddd93b6e3d
ZachCortez <Zach.Cortez@icloud.com>
2025-05-16 16:13:07 -0400

    final commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+import Navbar from "@/components/Navbar";
+
+const Layout = ({
+                    children,
+                }: Readonly<{
+    children: React.ReactNode;
+}>) => {
+    return (
+        <div>
+            <Navbar />
+            {children}
+        </div>
+    )
+}
+export default Layout
---
+++ app/(root)/page.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import Header from "@/components/Header";
+import VideoCard from "@/components/VideoCard";
+import {dummyCards} from "@/constants";
+import { getAllVideos } from "@/lib/actions/video"
+import EmptyState from "@/components/EmptyState";
+import Pagination from "@/components/Pagination";
+
+const Page = async ({ searchParams }: SearchParams) => {
+    const { query, filter, page } = await searchParams;
+
+    const { videos, pagination } = await getAllVideos(
+        query,
+        filter,
+        Number(page) || 1
+    );
+
+  return (
+      <main className="wrapper page">
+          <Header subHeader="Public Library" title="All Videos"/>
+
+          {videos?.length > 0 ? (
+              <section className="video-grid">
+                  {videos.map(({video, user}) => (
+                      <VideoCard
+                          key={video.id}
+                          id={video.videoId}
+                          title={video.title}
+                          thumbnail={video.thumbnailUrl}
+                          createdAt={video.createdAt}
+                          userImg={user?.image ?? ""}
+                          username={user?.name ?? "Guest"}
+                          views={video.views}
+                          visibility={video.visibility}
+                          duration={video.duration}
+                      />
+                  ))}
+              </section>
+          ) : (
+              <EmptyState
+                  icon="/assets/icons/video.svg"
+                  title="No Videos Found"
+                  description="Try adjusting your search."
+              />
+          )}
+
+          {pagination?.totalPages > 1 && (
+              <Pagination
+                  currentPage={pagination.currentPage}
+                  totalPages={pagination.totalPages}
+                  queryString={query}
+                  filterString={filter}
+              />
+          )}
+      </main>
+  )
+}
+
+export default Page
